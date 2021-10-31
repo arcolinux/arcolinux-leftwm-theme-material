bfb36349a21c51f006f3c2e5559dd29a7afa71d1
ArcoLinux <arcolinuxinfo@gmail.com>
2021-10-31 17:51:03 +0100

    more checks

--- scripts/scroll_spotify_status.sh
+++ scripts/scroll_spotify_status.sh
@@ -4,9 +4,9 @@
 zscroll -l 80 \
         --delay 0.1 \
         --scroll-padding "  " \
-        --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
+        --match-command "$HOME/.config/leftwm/themes/current/scripts/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
-        --update-check true "$HOME/.config/polybar/scripts/get_spotify_status.sh" &
+        --update-check true "$HOME/.config/leftwm/themes/current/scripts/get_spotify_status.sh" &
 
 wait
